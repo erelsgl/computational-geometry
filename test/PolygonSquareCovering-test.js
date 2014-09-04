a2f276f7fa9adcf4e91eddc1a3324ec3d3b5bf94
erelsgl <erelsgl@gmail.com>
2014-09-04 15:33:34 +0300

    square covering - all tests pass

--- test/PolygonSquareCovering-test.js
+++ test/PolygonSquareCovering-test.js
@@ -209,7 +209,7 @@ describe('square-covering structures', function() {
 		srp.corners.pluck("y").should.eql([00,00,40,40,50,50,90,90,50,50,40,40]);
 		srp.removeErasableRegion(srp.segments.last, 3 /* knobs */);
 		srp.corners.pluck("x").should.eql([10,30,30,40,40,00,00,10]);
-		srp.corners.pluck("y").should.eql([30,30,50,50,90,90,50,50]);
+		srp.corners.pluck("y").should.eql([40,40,50,50,90,90,50,50]);
 	});
 
 	
@@ -246,10 +246,35 @@ describe('minimal square-covering of ', function() {
 		jsts.minimalSquareCoveringOf([0,0, 10,35]).should.eql([ { minx: 0, maxx: 10, miny: 0, maxy: 10 },  { minx: 0, maxx: 10, miny: 10, maxy: 20 },  { minx: 0, maxx: 10, miny: 20, maxy: 30 },  { minx: 0, maxx: 10, miny: 25, maxy: 35 } ]);
 	});
 
-	it('L-shapes', function () {
-		jsts.minimalSquareCoveringOf([0,0, 10,10, 20,20]).should.eql([ { minx: 0, maxx: 10, miny: 0, maxy: 10 }, { minx: 10, maxx: 20, miny: 10, maxy: 20 }, { minx: 0, maxx: 10, miny: 10, maxy: 20 } ]);
-		jsts.minimalSquareCoveringOf([0,0, 15,5, 20,20]).should.eql([ { minx: 0, maxx: 15, miny: 0, maxy: 15 }, { minx: 5, maxx: 20, miny: 5, maxy: 20 }, { minx: 0, maxx: 15, miny: 5, maxy: 20 } ]);
-		jsts.minimalSquareCoveringOf([0,0, 5,15, 20,20]).should.eql([ { minx: 0, maxx: 5, miny: 0, maxy: 5 },{ minx: 0, maxx: 5, miny: 5, maxy: 10 },{ minx: 0, maxx: 5, miny: 10, maxy: 15 },{ minx: 15, maxx: 20, miny: 15, maxy: 20 },{ minx: 10, maxx: 15, miny: 15, maxy: 20 },{ minx: 5, maxx: 10, miny: 15, maxy: 20 },{ minx: 0, maxx: 5, miny: 15, maxy: 20 } ]);
-		jsts.minimalSquareCoveringOf([10,0, 20,20, 0,10]).should.eql([ { minx: 10, maxx: 20, miny: 0, maxy: 10 },{ minx: 10, maxx: 20, miny: 10, maxy: 20 },{ minx: 0, maxx: 10, miny: 10, maxy: 20 } ]);
+	it('L-shapes', function () { // also tests two types of a 1-knob
+		jsts.minimalSquareCoveringOf([0,0, 15,5, 20,20]).should.eql([ { minx: 0, maxx: 15, miny: 0, maxy: 15 }, { minx: 5, maxx: 20, miny: 5, maxy: 20 }, { minx: 0, maxx: 15, miny: 5, maxy: 20 } ]); // Fat L-shape: 3 overlapping squares
+		jsts.minimalSquareCoveringOf([0,0, 10,10, 20,20]).should.eql([ { minx: 0, maxx: 10, miny: 0, maxy: 10 }, { minx: 10, maxx: 20, miny: 10, maxy: 20 }, { minx: 0, maxx: 10, miny: 10, maxy: 20 } ]);   // Medium L-shape: 3 disjoint squares
+		jsts.minimalSquareCoveringOf([10,0, 20,20, 0,10]).should.eql([ { minx: 10, maxx: 20, miny: 0, maxy: 10 },{ minx: 10, maxx: 20, miny: 10, maxy: 20 },{ minx: 0, maxx: 10, miny: 10, maxy: 20 } ]);   // Medium L-shape: 3 disjoint squares
+		jsts.minimalSquareCoveringOf([0,0, 5,15, 20,20]).should.eql([ { minx: 0, maxx: 5, miny: 0, maxy: 5 },{ minx: 0, maxx: 5, miny: 5, maxy: 10 },{ minx: 0, maxx: 5, miny: 10, maxy: 15 },{ minx: 15, maxx: 20, miny: 15, maxy: 20 },{ minx: 10, maxx: 15, miny: 15, maxy: 20 },{ minx: 5, maxx: 10, miny: 15, maxy: 20 },{ minx: 0, maxx: 5, miny: 15, maxy: 20 } ]);  // Thin L-shape: 7 disjoint squares
+	});
+
+	it('2-knob continuators', function () {  // tests 2-knob continuators
+		jsts.minimalSquareCoveringOf([0,0, 5,5, 4,7]).should.eql([ { minx: 0, maxx: 5, miny: 0, maxy: 5}, { minx: 0, maxx: 4, miny: 3, maxy: 7}]); // type #1 knob;   should have 2 overlapping squares
+		jsts.minimalSquareCoveringOf([0,0, 5,5, 3,6, -1,3]).should.eql([ { minx: 0, maxx: 5, miny: 0, maxy: 5}, { minx: 0, maxx: 3, miny: 3, maxy: 6}, { minx: -1, maxx: 2, miny: 3, maxy: 6}]); // type #2 knob;   should have 3 overlapping squares
+	});
+
+	it('bottle-shapes', function () {  // tests the third type of a 1-knob
+		jsts.minimalSquareCoveringOf([0,0, 4,4, 3,5, 1,4]).should.eql([ { minx: 0, maxx: 4, miny: 0, maxy: 4}, { minx: 1, maxx: 3, miny: 3, maxy: 5}]); // Fat bottle-shape: 2 overlapping squares
+		jsts.minimalSquareCoveringOf([0,0, 3,3, 2,4, 1,3]).should.eql([ { minx: 0, maxx: 3, miny: 0, maxy: 3}, { minx: 1, maxx: 2, miny: 3, maxy: 4}]); // Medium bottle-shape: 2 disjoint squares
+		jsts.minimalSquareCoveringOf([1,3, 2,4, 3,7, 0,4]).should.eql([ { minx: 1, maxx: 2, miny: 3, maxy: 4}, { minx: 0, maxx: 3, miny: 4, maxy: 7}]); 
+	});
+
+	it('more bottle-shapes', function () {  // tests the third type of a 1-knob
+//		jsts.minimalSquareCoveringOf([0,0, 4,5, 3,8, 1,5]).should.eql([ { minx: 0, maxx: 4, miny: 0, maxy: 4}, { minx: 0, maxx: 4, miny: 1, maxy: 5}, { minx: 1, maxx: 3, miny: 5, maxy: 7}, { minx: 1, maxx: 3, miny: 6, maxy: 8}]); // Thin bottle-shape: 4 overlapping squares
+		jsts.minimalSquareCoveringOf([0,0, 4,5, 3,8, 1,4]).should.eql([ { minx: 0, maxx: 4, miny: 0, maxy: 4}, { minx: 1, maxx: 4, miny: 2, maxy: 5}, { minx: 1, maxx: 3, miny: 5, maxy: 7}, { minx: 1, maxx: 3, miny: 6, maxy: 8}]); // Thin asymmetric bottle-shape: 4 overlapping squares
+	});
+
+	it('room-shapes', function () {   // tests 3-knob continuaturs
+		jsts.minimalSquareCoveringOf([0,0, 4,4, 3,5, 4,9, 0,5, 1,4]).should.eql([ { minx: 0, maxx: 4, miny: 0, maxy: 4}, { minx: 1, maxx: 3, miny: 4, maxy: 6}, { minx: 0, maxx: 4, miny: 5, maxy: 9 } ]); // Fat room-shape: 3 overlapping squares
+		jsts.minimalSquareCoveringOf([0,0, 3,3, 2,4, 3,7, 0,4, 1,3]).should.eql([ { minx: 0, maxx: 3, miny: 0, maxy: 3}, { minx: 1, maxx: 2, miny: 3, maxy: 4}, { minx: 0, maxx: 3, miny: 4, maxy: 7 } ]); // Medium room-shape: 3 disjoint squares
+	});
+
+	it('1-knobs', function () {  // tests the third type of a 1-knob
+		jsts.minimalSquareCoveringOf([0,0, 5,6, 4,8, 1,7]).should.eql([ { minx: 0, maxx: 5, miny: 0, maxy: 5 }, { minx: 0, maxx: 5, miny: 1, maxy: 6 }, { minx: 0, maxx: 4, miny: 3, maxy: 7 }, { minx: 1, maxx: 4, miny: 5, maxy: 8 } ]); // Fat L-shape: 3 overlapping squares
 	});
 });
